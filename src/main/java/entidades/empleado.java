98ba7c6075b590e60a19881372e68920fff8934b
Mauricio Uñate <mauriciounate@gmail.com>
2022-11-14 17:03:26 -0500

    creado paquetes y clases

---
+++ src/main/java/entidades/empleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author mauriciounate
+ */
+public class empleado {
+    
+    String IdPersona, cargo, sueldo, fechaIngreo;
+    
+}
---
+++ src/main/java/entidades/persona.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author mauriciounate
+ */
+public class persona {
+    
+    String IdPersona, Pnombre, Snombre, Papellido, Sapellido,telefono, sexo, fechaNacimiento,tipoDoc, NumDoc, estado;
+}
